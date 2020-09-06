fd0685872a9ddb458ba60df57fbfbba4334bef6c
briannalturner <blturner3527@gmail.com>
2020-09-06 19:52:01 -0400

    connecting routes

--- angular/dogs-app/src/app/app.module.ts
+++ angular/dogs-app/src/app/app.module.ts
@@ -5,23 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ConvertSpaces } from './product-list/convertSpaces.component'
+import { ConvertSpaces } from './shared/convertSpaces.component'
 import { StarComponent } from './shared/star.component'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-list/product-detail.component'
+import { RouterModule } from '@angular/router'
+
  
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertSpaces,
-    StarComponent
+    StarComponent,
+    HomeComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
